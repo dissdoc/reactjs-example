a73c2868f4da90915ae5122c04de26529b00d85f
Kirill Mukhov <dissdoc@gmail.com>
2016-09-23 17:32:51 +0300

    add rendering

--- src/js/main.js
+++ src/js/main.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class Layout extends React.Component {
+	constructor() {
+		super();
+		this.message = "It works";
+	}
+
 	render() {
+		const name = "Dissdoc";
+
 		return (
-			<h1>It works!</h1>
+			<h1>{this.message}, {name}!</h1>
 		);	
 	}
 }
