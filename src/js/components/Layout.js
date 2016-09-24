afb1790d8ee5dd70f05c53e6d78620ebfd41fc8e
Kirill Mukhov <dissdoc@gmail.com>
2016-09-25 01:17:26 +0300

    add router

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 import Header from './Header';
 import Footer from './Footer';
@@ -16,16 +17,26 @@ export default class Layout extends React.Component {
 		this.setState({title});
 	}
 
+	navigate() {
+		this.props.history.pushState(null, '/');	
+	}
+
 	render() {
 		setTimeout(() => {
 			this.setState({title: 'Welcome!'});
 		}, 2000);
 
 		return (
-			<ul>
+			<div>
 				<Header changeTitle={this.changeTitle.bind(this)} title={this.state.title} />
+
+				{ this.props.children }
+				<Link to="archives">archives</Link>
+				<Link to="settings">settings</Link>
+				<button onClick={this.navigate.bind(this)}>Featured</button>
+
 				<Footer />
-			</ul>			
+			</div>			
 		);	
 	}
 }
