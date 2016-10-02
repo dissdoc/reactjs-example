2e688de950ce67e41bc167960065303f8b80fe19
Kirill Mukhov <dissdoc@gmail.com>
2016-10-02 20:53:18 +0300

    add stylus

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,18 @@
  'use strict';
 
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const rimraf = require('rimraf');
+
  var debug = process.env.NODE_ENV !== 'production',
      webpack = require('webpack');
 
  module.exports = {
  	context: __dirname + '/src',
- 	entry: './js/main.js',
+ 	entry: {
+ 		main: './js/main',
+ 		styles: './styles'
+ 	},
  	
  	module: {
  		loaders: [
@@ -17,24 +24,48 @@
 	 				presets: ['react', 'es2015', 'stage-0'],
 	 				plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
 	 			}	
-	 		}
+	 		},
+	 		{
+		      test:   /\.styl$/,
+		      loader: ExtractTextPlugin.extract('css!stylus?resolve url')
+		    }
  		]
  	},
  	
  	output: {
- 		path: __dirname + '/src/',
+ 		path: __dirname + '/public',
+ 		publicPath: '/',
  		filename: 'app.min.js'
  	},
 
+ 	resolve: {
+ 		extensions: ['', '.js', '.styl']
+ 	},
+
  	plugins: debug ? [
- 		new webpack.OldWatchingPlugin()
+ 		new webpack.OldWatchingPlugin(),
+ 		new ExtractTextPlugin('styles.css', {allChunks: true}),
+ 		new HtmlWebpackPlugin({
+ 			template: 'index.html',
+ 			filename: './index.html'
+ 		})
  	] : [
+ 		new HtmlWebpackPlugin({
+ 			template: 'index.html',
+ 			filename: './index.html'
+ 		}),
+ 		new ExtractTextPlugin('styles.css', {allChunks: true}),
  		new webpack.optimize.DedupePlugin(),
  		new webpack.optimize.OccurenceOrderPlugin(),
  		new webpack.optimize.UglifyJsPlugin({
  			mangle: false,
  			sourcemap: false
- 		})
+ 		}),
+ 		{
+	      apply: (compiler) => {
+	        rimraf.sync(compiler.options.output.path);
+	      }
+	    }
  	],
 
  	devServer: {
